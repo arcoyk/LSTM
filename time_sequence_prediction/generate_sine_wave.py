118aaa79f52805da33571c6b23ec640da44f48de
yui <yuikita21@gmail.com>
2017-12-12 19:17:26 +0900

    fix

--- time_sequence_prediction/generate_sine_wave.py
+++ time_sequence_prediction/generate_sine_wave.py
@@ -5,8 +5,8 @@ import torch
 
 np.random.seed(2)
 
-T = 20 # Randomness
-L = 1000 # Number of columns
+T = 15 # Randomness
+L = 100 # Number of columns
 N = 100 # Number of rows
 
 x = np.empty((N, L), 'int64')
@@ -16,5 +16,6 @@ x = np.empty((N, L), 'int64')
 # Shift [1,2,3,4...L] to random(-20, 20)
 x[:] = np.array(range(L)) + np.random.randint(-4 * T, 4 * T, N).reshape(N, 1)
 # Convert each column to sin
-data = np.sin(x / 1.0 / T).astype('float64')
+# data = np.sin(x / 1.0 / T).astype('float64')
+data = np.sin(x / T).astype('float64')
 torch.save(data, open('traindata.pt', 'wb'))
--- time_sequence_prediction/train.py
+++ time_sequence_prediction/train.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 import torch.optim as optim
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 class Sequence(nn.Module):
@@ -13,18 +13,17 @@ class Sequence(nn.Module):
         super(Sequence, self).__init__()
         # LSTMCell(input_size, hidden_size)
         # Why?
-        self.lstm1 = nn.LSTMCell(1, 51)
-        self.lstm2 = nn.LSTMCell(51, 1)
+        self.L = 100
+        self.lstm1 = nn.LSTMCell(1, self.L)
+        self.lstm2 = nn.LSTMCell(self.L, 1)
 
     def forward(self, input, future = 0):
         outputs = []
-        h_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
-        c_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        h_t = Variable(torch.zeros(input.size(0), self.L).double(), requires_grad=False)
+        c_t = Variable(torch.zeros(input.size(0), self.L).double(), requires_grad=False)
         h_t2 = Variable(torch.zeros(input.size(0), 1).double(), requires_grad=False)
         c_t2 = Variable(torch.zeros(input.size(0), 1).double(), requires_grad=False)
         for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
-            print(input_t)
-            exit()
             # 3 input and 2 output
             h_t, c_t = self.lstm1(input_t, (h_t, c_t))
             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
@@ -47,10 +46,11 @@ if __name__ == '__main__':
     # 0.1, 0.2, 0.4, 0.1, 0.3, 0.5
     # <------- input ------->
     #      <------ target ------->
-    input = Variable(torch.from_numpy(data[3:, :-1]), requires_grad=False)
-    target = Variable(torch.from_numpy(data[3:, 1:]), requires_grad=False)
-    test_input = Variable(torch.from_numpy(data[:3, :-1]), requires_grad=False)
-    test_target = Variable(torch.from_numpy(data[:3, 1:]), requires_grad=False)
+    LL = 50
+    input = Variable(torch.from_numpy(data[:LL, :-1]), requires_grad=False)
+    target = Variable(torch.from_numpy(data[:LL, 1:]), requires_grad=False)
+    test_input = Variable(torch.from_numpy(data[LL:, :-1]), requires_grad=False)
+    test_target = Variable(torch.from_numpy(data[LL:, 1:]), requires_grad=False)
     # build the model
     # This could be RNN
     seq = Sequence()
@@ -60,7 +60,6 @@ if __name__ == '__main__':
     # MSE stands for Mean Square Error
     criterion = nn.MSELoss()
     # use pytorch.optim.LBFGS as optimizer since we can load the whole data to train
-    # seq
     optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
     #begin to train
     # 15 iterations
@@ -75,23 +74,19 @@ if __name__ == '__main__':
             return loss
         optimizer.step(closure)
         # begin to predict
-        future = 1000
+        future = 100
         pred = seq(test_input, future = future)
         loss = criterion(pred[:, :-future], test_target)
         print('test loss:', loss.data.numpy()[0])
-        y = pred.data.numpy()
+        # y = pred.data.numpy()
         # draw the result
-        plt.figure(figsize=(30,10))
-        plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
-        plt.xlabel('x', fontsize=20)
-        plt.ylabel('y', fontsize=20)
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        def draw(yi, color):
-            plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
-            plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
-        draw(y[0], 'r')
-        draw(y[1], 'g')
-        draw(y[2], 'b')
+        # plt.plot(input.data[i].numpy())
+        # plt.plot(pred.data[3].numpy())
+        for m in range(10):
+            past = test_input.data[m].numpy()
+            post = pred.data[m].numpy()
+            past = list(past) + list(post)
+            plt.plot(past)
+        plt.show()
         plt.savefig('predict%d.pdf'%i)
         plt.close()
